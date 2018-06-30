fa615333bce464e4d7ae426bc12ad9fa72aa8a82
Matt Kirby <MattKir@aol.com>
2018-06-30 11:50:07 -0700

    Finishing MVP

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ $("document").ready(launchApp);
 
 var firstCardClicked = null;
 var secondCardClicked = null;
-var totalPossibleMatches = 2;
+var totalPossibleMatches = 9;
 var matchCounter = 0;
 var clickable = true;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
 
 function launchApp() {
     $(".card").on("click", cardFlip);
+    startStats();
 }
 
 function cardFlip() {
@@ -30,14 +35,17 @@ function cardFlip() {
         if(firstCardClicked === secondCardClicked) {
             clickable = true;
             matchCounter++;
+            matches++;
             firstCardClicked, secondCardClicked = null;
+            displayStats();
             if (matchCounter === totalPossibleMatches) {
                 $("#game-area").append($("<h1>").html("You Win"));
             } else {
-                console.log("didn't win");
+                console.log("didn't win yet");
             }
         }else{
             setTimeout(flipBack, 2000);
+            displayStats();
         }
 
     }
@@ -49,5 +57,29 @@ function flipBack() {
     firstCardClicked = null;
     secondCardClicked = null;
     clickable = true;
+}
+
+function startStats() {
+    ++games_played;
+    $(".gamesPlayed.value").text(games_played);
+    $(".attempts.value").text(attempts);
+    $(".accuracy.value").text(accuracy + "%");
+}
+
+function displayStats() {
+    $(".gamesPlayed.value").text(games_played);
+    $(".attempts.value").text(attempts);
+    var accuracyPercentage = Math.round((matches/attempts) * 100) + "%";
+    $(".accuracy.value").text(accuracy);
+}
+
+function reset() {
+    console.log("reset game");
+    firstCardClicked = null;
+    secondCardClicked = null;
+    totalPossibleMatches = 9;
+    matchCounter = 0;
+    attempts = 0;
+    matches = 0;
 }
 
